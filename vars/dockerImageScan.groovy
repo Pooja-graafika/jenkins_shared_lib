3e189ecc26d90a1f9d77197ab3964fb4f75a3b01
Pooja-graafika <84763722+Pooja-graafika@users.noreply.github.com>
2023-05-08 03:42:44 -0400

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -9,14 +9,7 @@
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
-    trivy image -f json -o scan.json ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest 
-    cat scan.json
-                        def vulnerabilities = readJSON(text: scan.json)
-                    if (vulnerabilities.length > 0) {
-                        // If vulnerabilities are found, fail the pipeline
-                        error "Trivy scan found ${vulnerabilities.length} vulnerabilities"
-						
-						
-						}
+    trivy image -f template -o scan.html ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest 
+    cat scan.html
     """
 }
