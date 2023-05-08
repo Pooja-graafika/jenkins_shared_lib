a5f41622888c46ce10f3e10cbe4c1bcade38adf8
Pooja-graafika <84763722+Pooja-graafika@users.noreply.github.com>
2023-05-08 04:02:16 -0400

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -9,7 +9,7 @@
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
-    trivy image -f template -o scan.html ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest 
-    cat scan.html
+    trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
+    cat scan.txt
     """
 }
